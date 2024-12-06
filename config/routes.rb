ebf467e1eb4dcc81230e66044bda11405d3c7e85
Umar Hayat <umar@DESKTOP-N2UQP56.localdomain>
2024-12-06 21:54:41 +0500

    Made Posts controller Now i will start the editing process layouting

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :posts
   root "pages#index"
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
-
+  get "/posts", to: "posts#index"
 
 
   get "up" => "rails/health#show", as: :rails_health_check
